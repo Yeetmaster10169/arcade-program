61505e3a56c8054fc64babde487b48919def5161
gopalasaketh@gmail.com <gopalasaketh@gmail.com>
2023-11-13 13:17:47 +0000

    Edited Main

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -2,7 +2,6 @@ from ._anvil_designer import MainTemplate
 from anvil import *
 import anvil.server
 
-
 class Main(MainTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
